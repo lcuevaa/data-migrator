1a79f6a5ad35c751b9e3b970a1b0ce7444c1f47a
lcuevaa <luisdeynercuevaaltamirano@gmail.com>
2025-02-10 15:12:05 -0500

    migración de datos de productos

--- database/migration.py
+++ database/migration.py
@@ -1,12 +1,14 @@
 from migrations.empresas import migrate_empresas
 from migrations.sucursales import migrate_sucursales
-
+from migrations.productos import migrate_productos
 def migrate_data():
     print("Iniciando la migracion de datos...")
     
     # Migrar cada tabla/vista de forma independiente
     migrate_empresas()
     migrate_sucursales()
+    migrate_productos()
+    
     
     print("Migración de datos finalizada.")
 
---
+++ migrations/productos.py
@@ -0,0 +1,35 @@
+from database.sql_server import fetch_sql_data
+from database.postgres import insert_postgres_data
+
+def migrate_productos():
+    print("Migrando vista PRODUCTOS...")
+
+    # Consulta de datos en SQL Server
+    sql_query = "SELECT * FROM VIEW_PRODUCTOS"
+    rows = fetch_sql_data(sql_query)
+
+    if not rows:
+        print("No se encontraron datos nuevos para migrar en productos")
+        return
+
+    # Inserción en PostgreSQL
+    insert_query = """
+        INSERT INTO productos (
+            producto_id,
+            producto_codigo,
+            codigo_sunat,
+            empresa,
+            descripcion,
+            unidad,
+            peso,
+            tipo_afectacion
+        )
+        VALUES %s
+        ON CONFLICT (producto_id) DO NOTHING
+    """
+    inserted_count = insert_postgres_data(insert_query, rows)
+
+    if inserted_count == 0:
+        print("No se encontraron datos nuevos para migrar en productos")
+    else:
+        print(f"Migrados {inserted_count} registros de productos")
